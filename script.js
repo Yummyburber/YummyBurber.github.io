17ec6f304223c272a9e67aea881312c8af22a41d
YummyBurber <BlessedLukey@gmail.com>
2026-05-06 15:04:27 -0500

    restore files

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-window.onbeforeunload = function () {
-window.scrollTo(0,0)
-};
+history.scrollRestoration ='manual';
+window.scrollTo(0,0);
+
+function fixPadding() {
+const headerHeight = document.querySelector('header').offsetHeight
+document.body.style.paddingTop = headerHeight + 'px';
+}
+
+fixPadding();
+window.addEventListener('resize',fixPadding);
