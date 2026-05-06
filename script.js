44311ffc018e56cbd93c4ac55c2b79d10767b4e6
YummyBurber <BlessedLukey@gmail.com>
2026-05-06 14:54:57 -0500

    Revert "add images"
    This reverts commit 842b9af78dbb085f95bcd024a3f8367f73d121d8.

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-history.scrollRestoration ='manual';
-window.scrollTo(0,0);
-
-function fixPadding() {
-const headerHeight = document.querySelector('header').offsetHeight
-document.body.style.paddingTop = headerHeight + 'px';
-}
-
-fixPadding();
-window.addEventListener('resize',fixPadding);
+window.onbeforeunload = function () {
+window.scrollTo(0,0)
+};
